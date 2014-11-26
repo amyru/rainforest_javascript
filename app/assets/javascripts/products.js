300288395331ee63402ea120603d3946181ebbb6
Amy Rudolph <amy.rudolph@live.co.uk>
2014-11-26 14:52:07 -0500

    added a respond_to in reviews controller to use ajax when creating reviews. Added remote => true in create review form.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,5 +1,3 @@
-
-
 $(document).on('ready page:load', function() {
   $('#search-form').submit(function(event) {
   	event.preventDeafault();
@@ -20,22 +18,3 @@ $(document).on('ready page:load', function() {
   });
 });
 
-
-
-// (document).on('page:load ready', function() {
-//   $('#search-form').submit(function(event) {
-//     event.preventDefault();
-//     var searchValue = $('#search').val();
-
-//     $.getScript('/products?search=' + searchValue);
-//   });
-
-//   $(window).scroll(function() {
-//     var url = $('.pagination span.next').children().attr('href');
-//     if (url && $(window).scrollTop() > ($(document).height() - $(window).height() - 50)) {
-
-//       console.log(url);
-//       $('#product-pagination').text("Fetching more products...");
-//       $.getScript(url);
-//     }
-//   });
